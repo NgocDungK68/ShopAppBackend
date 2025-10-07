98d6270925be5300f9ff06071e9ef34325bccaaf
NgocDungK68 <dungtranngoc1412@gmail.com>
2025-10-07 22:10:34 +0700

    code order

---
+++ src/main/java/com/project/shopapp/controller/OrderController.java
@@ -0,0 +1,56 @@
+package com.project.shopapp.controller;
+
+import com.project.shopapp.dto.OrderDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/${api.prefix}/orders")
+public class OrderController {
+    @PostMapping("")
+    public ResponseEntity<?> createOrder(
+            @Valid @RequestBody OrderDTO orderDTO,
+            BindingResult result
+    ) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok("createOrder successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<?> getOrders(@Valid @PathVariable("user_id") Long user_id) {
+        try {
+            return ResponseEntity.ok("Lấy ra danh sách orders từ user_id");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrder(
+            @Valid @PathVariable Long id,
+            @Valid @RequestBody OrderDTO orderDTO
+    ) {
+        return ResponseEntity.ok("updateOrder successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> updateOrder(@Valid @PathVariable Long id) {
+        // xoá mềm
+        return ResponseEntity.ok("deleteOrder successfully");
+    }
+}
